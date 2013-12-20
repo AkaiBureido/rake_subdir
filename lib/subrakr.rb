cb6cb5af86bec3f041b2a500ee124747c06a1e79
AkaiBureido <utkin.oleg@me.com>
2013-12-20 08:47:11 +0400

    Adding the DSL encapsulation

--- lib/subrakr.rb
+++ lib/subrakr.rb
@@ -1,20 +1,22 @@
 require "subrakr/version"
 
 module Rake
-  def rake_subdir(dirs)
-    dirs = { dirs => '' } if dirs.class == String
-    dirs.each_pair do |dir, tasks|
-      subrakefile = (Pathname.new(dir) + 'Rakefile').to_s
-      raise "Error #{subrakefile} does not exist" unless File.exists?(subrakefile)
-      Dir.chdir(dir) do
-        puts "-> Executing [#{subrakefile}] (#{tasks})"
-        out = %x{rake #{tasks} 2>&1}
-        puts out.gsub(/^/,'   ') unless out.empty?
-        raise "Error while executing (#{tasks}) in [#{subrakefile}]" if $?.exitstatus != 0
+  module DSL
+    def rake_subdir(dirs)
+      dirs = { dirs => '' } if dirs.class == String
+      dirs.each_pair do |dir, tasks|
+        subrakefile = (Pathname.new(dir) + 'Rakefile').to_s
+        raise "Error #{subrakefile} does not exist" unless File.exists?(subrakefile)
+        Dir.chdir(dir) do
+          puts "-> Executing [#{subrakefile}] (#{tasks})"
+          out = %x{rake #{tasks} 2>&1}
+          puts out.gsub(/^/,'   ') unless out.empty?
+          raise "Error while executing (#{tasks}) in [#{subrakefile}]" if $?.exitstatus != 0
+        end
       end
+    rescue StandardError => e
+      puts e
+      exit(1)
     end
-  rescue StandardError => e
-    puts e
-    exit(1)
   end
 end
